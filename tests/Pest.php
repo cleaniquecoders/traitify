b15374847e7537b75822d28bfc47a1634035bc17
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-11-27 10:31:12 +0800

    Added listRoutes() helper - for debugging purpose

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,3 +3,31 @@
 use CleaniqueCoders\Traitify\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+
+use Illuminate\Support\Facades\Route;
+
+if (! function_exists('listRoutes')) {
+    /**
+     * List all registered routes for debugging in a structured format.
+     *
+     * @param  bool  $dump  Whether to dump the routes to the console.
+     * @return \Illuminate\Support\Collection A collection of routes with details.
+     */
+    function listRoutes(bool $dump = false)
+    {
+        $routes = collect(Route::getRoutes())->map(function ($route) {
+            return [
+                'uri' => $route->uri(),
+                'name' => $route->getName(),
+                'action' => $route->getActionName(),
+                'methods' => $route->methods(),
+            ];
+        });
+
+        if ($dump) {
+            dump($routes); // Dumps routes to the console
+        }
+
+        return $routes;
+    }
+}
